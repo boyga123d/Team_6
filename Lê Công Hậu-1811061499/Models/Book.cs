a6df92602900a6741b15dede483a1a560eea921c
boyga123d <72071157+boyga123d@users.noreply.github.com>
2021-06-25 11:26:40 +0700

    Add project files.

---
+++ Lê Công Hậu-1811061499/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _1811061499.Models
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+        public String BookName { get; set; }
+        public int Page { get; set; }
+    }
+}
---
+++ Lê Công Hậu-1811061499/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lê_Công_Hậu_1811061499.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
