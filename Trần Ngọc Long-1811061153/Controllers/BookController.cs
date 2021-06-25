ce8168d88490db186bc3427c51e0366113d3e407
boyga123d <72071157+boyga123d@users.noreply.github.com>
2021-06-25 11:24:39 +0700

    Add project files.

---
+++ Trần Ngọc Long-1811061153/Controllers/BookController.cs
@@ -0,0 +1,26 @@
+﻿using _1811061153.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _1811061153.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            var BookList = new List<Book>
+            {
+                new Book(){BookID = 1, BookName = "A", Page = 10},
+                new Book(){BookID = 1, BookName = "A", Page = 10},
+                new Book(){BookID = 1, BookName = "A", Page = 10},
+                new Book(){BookID = 1, BookName = "A", Page = 10},
+                new Book(){BookID = 1, BookName = "A", Page = 10},
+                new Book(){BookID = 1, BookName = "A", Page = 10},
+            };
+            return View(BookList);
+        }
+    }
+}
---
+++ Trần Ngọc Long-1811061153/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Trần_Ngọc_Long_1811061153.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
